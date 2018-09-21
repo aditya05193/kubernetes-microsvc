d70efd5ad1554cb58d81bbcf409778829bf0eb47
aditya05193 <42135669+aditya05193@users.noreply.github.com>
2018-09-21 05:19:41 -0400

    Update docker-build.sh

--- microservice-kubernetes-demo/docker-build.sh
+++ microservice-kubernetes-demo/docker-build.sh
@@ -2,23 +2,11 @@
 if [ -z "$DOCKER_ACCOUNT" ]; then
     DOCKER_ACCOUNT=aditya05193
 fi;
-docker build --tag=microservice-kubernetes-demo-apache apache
-docker tag microservice-kubernetes-demo-apache $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache
-
-docker build --tag=microservice-kubernetes-demo-catalog microservice-kubernetes-demo-catalog
-docker tag microservice-kubernetes-demo-catalog $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:latest
+docker build --tag=$DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:v1 microservice-kubernetes-demo-catalog
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog
 
-docker build --tag=microservice-kubernetes-demo-customer microservice-kubernetes-demo-customer
-docker tag microservice-kubernetes-demo-customer $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:latest
+docker build --tag=$DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:v1 microservice-kubernetes-demo-customer
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer
 
-docker build --tag=microservice-kubernetes-demo-order microservice-kubernetes-demo-order
-docker tag microservice-kubernetes-demo-order $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:latest
+docker build --tag=$DOCKER_ACCOUNT/microservice-kubernetes-demo-order:v1 microservice-kubernetes-demo-order
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-order
-
-docker build --tag=microservice-kubernetes-demo-hystrix-dashboard microservice-kubernetes-demo-hystrix-dashboard
-docker tag microservice-kubernetes-demo-hystrix-dashboard $DOCKER_ACCOUNT/microservice-kubernetes-demo-hystrix-dashboard:latest
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-hystrix-dashboard
-
